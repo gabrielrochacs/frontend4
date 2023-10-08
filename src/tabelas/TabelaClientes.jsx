6c3eeaaf97baeabe14c453cc6d94ea5c494e8558
Gsrcordeiro <cordeirogb@hotmail.com>
2023-10-08 16:00:52 -0300

    Clientes

--- src/tabelas/TabelaClientes.jsx
+++ src/tabelas/TabelaClientes.jsx
@@ -6,7 +6,7 @@ export default function TabelaClientes(props) {
 
     function filtrarClientes(e) {
         const termoBusca = e.currentTarget.value.toLowerCase();
-        fetch(urlBase + "/cliente", { method: "GET" })
+        fetch('http://129.146.68.51/aluno17-pfsii/clientes', { method: "GET" }) // Usando o URL base completo
             .then((resposta) => resposta.json())
             .then((listaClientes) => {
                 if (Array.isArray(listaClientes)) {
