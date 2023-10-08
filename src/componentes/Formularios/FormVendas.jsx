23eacc2bb58f268bd717b92ff264e230de70f479
Gsrcordeiro <cordeirogb@hotmail.com>
2023-10-08 14:31:06 -0300

    Ok

---
+++ src/componentes/Formularios/FormVendas.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { urlBase } from '../../utilitarios/definicoes';
+
+export default function FormVendas(props) {
+    const [venda, setVenda] = useState(props.venda);
+    const [clientes, setClientes] = useState([]);
+
+    useEffect(() => {
+        // Carregar a lista de clientes do banco de dados ou de onde quer que você obtenha os dados
+        fetch(urlBase + "/clientes", {
+            method: "GET"
+        })
+            .then((resposta) => {
+                return resposta.json();
+            }).then((dados) => {
+                if (Array.isArray(dados)) {
+                    setClientes([...dados]);
+                }
+            })
+            .catch((erro) => {
+                console.error("Erro ao buscar os dados dos clientes: " + erro);
+            });
+    }, []);
+
+    function manipulaMudanca(e) {
+        const { id, value } = e.target;
+        setVenda({ ...venda, [id]: value });
+    }
+
+    async function manipulaSubmissao(e) {
+        e.preventDefault();
+
+        if (validarCampos()) {
+            const metodo = props.modoEdicao ? 'PUT' : 'POST';
+            const endpoint = props.modoEdicao ? `/venda` : '/venda';
+
+            try {
+                const resposta = await fetch(urlBase + endpoint, {
+                    method: metodo,
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(venda),
+                });
+
+                const dados = await resposta.json();
+
+                if (dados.status) {
+                    if (!props.modoEdicao) {
+                        const novaVenda = { ...venda, id: dados.id };
+                        props.setVendas([...props.listaVendas, novaVenda]);
+                    } else {
+                        const listaAtualizada = props.listaVendas.map((item) =>
+                            item.id === venda.id ? venda : item
+                        );
+                        props.setVendas(listaAtualizada);
+                    }
+
+                    props.exibirTabela(true);
+                    window.alert('Venda salva com sucesso!');
+                } else {
+                    window.alert(dados.mensagem);
+                }
+            } catch (erro) {
+                window.alert('Erro ao executar a requisição: ' + erro.message);
+            }
+        }
+    }
+
+    function validarCampos() {
+        const camposObrigatorios = ['data', 'valor', 'cliente_id'];
+        for (const campo of camposObrigatorios) {
+            if (!venda[campo]) {
+                window.alert(`O campo "${campo}" é obrigatório.`);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    return (
+        <Form onSubmit={manipulaSubmissao}>
+            <Row>
+                <Col>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Data:</Form.Label>
+                        <Form.Control type="date" value={venda.data} id='data' onChange={manipulaMudanca} required />
+                        <Form.Control.Feedback type='invalid'>
+                            Por favor, informe a data.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Valor:</Form.Label>
+                        <Form.Control type="text" placeholder="Informe o valor" value={venda.valor} id='valor' onChange={manipulaMudanca} required />
+                        <Form.Control.Feedback type='invalid'>
+                            Por favor, informe o valor.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Cliente:</Form.Label>
+                        <Form.Control as="select" value={venda.cliente_id} id='cliente_id' onChange={manipulaMudanca} required>
+                            <option value="">Selecione um cliente</option>
+                            {clientes.map((cliente) => (
+                                <option key={cliente.cpf} value={cliente.cpf}>
+                                    {cliente.cpf}
+                                </option>
+                            ))}
+                        </Form.Control>
+                        <Form.Control.Feedback type='invalid'>
+                            Por favor, selecione um cliente.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Button variant="primary" type="submit">
+                        {props.modoEdicao ? 'Atualizar' : 'Salvar'}
+                    </Button>{' '}
+                    {!props.modoEdicao && (
+                        <Button variant="secondary" onClick={() => props.exibirTabela(true)}>
+                            Cancelar
+                        </Button>
+                    )}
+                </Col>
+            </Row>
+        </Form>
+    );
+}
---
+++ src/componentes/Telas/TelaVendas.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import Paginas from '../../Paginas/PaginaComponente';
+import FormVendas from '../Formularios/FormVendas.jsx';
+import TabelaVendas from '../../tabelas/TabelaVendas';
+import { Container, Alert } from 'react-bootstrap';
+import { urlBase } from '../../utilitarios/definicoes';
+
+export default function TelaVendas(props) {
+    const [vendas, setVendas] = useState([]);
+    const [exibirTabela, setExibirTabela] = useState(true);
+    const [atualizando, setAtualizando] = useState(false);
+    const [atualizarTabela, setAtualizarTabela] = useState(false);
+    const [vendaEdicao, setVendaEdicao] = useState({
+        id: "",
+        data: "",
+        valor: "",
+        cliente_id: "",
+    });
+
+    function preparaVenda(venda) {
+        setAtualizando(true);
+        setVendaEdicao(venda);
+        setExibirTabela(false);
+    }
+
+    function excluirVenda(venda) {
+        fetch(urlBase + '/venda', {
+            method: "DELETE",
+            headers: { "Content-Type": 'application/json' },
+            body: JSON.stringify(venda)
+        }).then((resposta) => {
+            return resposta.json();
+        }).then((retorno) => {
+            setAtualizarTabela(true);
+            window.alert('Dados da venda apagados com sucesso!');
+            window.location.reload();
+            if (retorno.resultado) {
+                console.log('  ');
+            } else if (retorno.resultado === false) {
+                window.alert('Não foi possível apagar os dados da venda!');
+            }
+        });
+    }
+
+    useEffect(() => {
+        fetch(urlBase + "/venda", {
+            method: "GET"
+        })
+            .then((resposta) => {
+                return resposta.json();
+            }).then((dados) => {
+                if (Array.isArray(dados)) {
+                    setVendas([...dados]);
+                }
+            })
+            .catch((erro) => {
+                console.error("Erro ao buscar os dados do banco: " + erro);
+            });
+        setExibirTabela(true);
+    }, []);
+
+    return (
+        <Paginas>
+            <Container className="border m-3">
+                <Alert variant={"secondary"} className='text-center'>Vendas</Alert>
+                {
+                    exibirTabela ?
+                        <TabelaVendas listaVendas={vendas} exibirTabela={setExibirTabela} setVendas={setVendas} editarVenda={preparaVenda} atualizarTabela={atualizarTabela} setAtualizarTabela={setAtualizarTabela} excluirVenda={excluirVenda} />
+                        :
+                        <FormVendas listaVendas={vendas} exibirTabela={setExibirTabela} setVendas={setVendas} modoEdicao={atualizando} venda={vendaEdicao} />
+                }
+            </Container>
+        </Paginas>
+    );
+}
